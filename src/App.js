c4078f2c743daa877f1059869beb5c10003ae536
Tanishq Shaikh <tanishq.s@somaiya.edu>
2022-01-31 01:58:20 -0500

    login component image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import './App.css';
-import Container from './container/Container';
+// import './App.css';
+// import Container from './container/Container';
+import SignInSide from './components/LoginComponent';
 
 const App = () => {
   return (
     <div className="App cfb">
-      <Container/>
+      {/* <Container/> */}
+      <SignInSide />
+
+
     </div>
   );
 }
