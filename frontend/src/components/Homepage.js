6da80644f2a17fae1b7d5edff8756236510ab908
Shreya Kasturia <70463887+shreyakasturia@users.noreply.github.com>
2022-03-18 13:42:05 -0400

    formatted components and added scroll to top button

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import vid from '../assets/bgvid.mp4';
 import {Button,Grid,Container,Typography,Box,Divider} from '@mui/material';
 import Footer from './Footer';
-export default function Login(){
-  return(
+export default function Login(){ 
+  return(       
   <div>
   <video autoPlay loop muted id="video" style={{
                 aspectRatio: 1,
@@ -12,15 +12,18 @@ export default function Login(){
   <source src={vid}></source>
   </video>
   <Container>
+    <br/>
+  
   <div class="Login">
-      <Typography variant="h3" color="black"> Get Started</Typography><br/>
-      <Typography variant="h5" color="black"> Sign Up/ Sign In</Typography><br/> <br/>
-  <a href="/signup"><Button variant="contained" disableElevation>Sign Up</Button></a> <br/>
-  <Typography variant="body3" color="black"> New User? Create a Free Account</Typography><br/> <br/>
+  <Typography variant="h4" color="black" align="center"> Let opportunity</Typography><br/>
+  <Typography variant="h2" color="#5781DB " align="center"> come to you</Typography><br/>
+  <br/>
+  <a href="/signup"><div align="center"><Button variant="contained" disableElevation>Sign Up</Button></div></a> <br/>
+  <div align="center"><Typography variant="body3" color="black"> New User? Create a Free Account</Typography></div><br/> <br/>
   </div>
   <div class="Login">
-  <a href="/signin"><Button variant="contained" disableElevation>Sign In</Button></a> <br/>
-  <Typography variant="body3" color="black"> Already Existing User</Typography><br/> <br/>
+  <a href="/signin"><div align="center"><Button variant="contained" disableElevation>Sign In</Button></div></a> <br/>
+  <div align="center"><Typography variant="body3" color="black"> Already Existing User</Typography></div><br/> <br/>
   </div>
   </Container>
   <Box
@@ -37,8 +40,8 @@ export default function Login(){
        <br/>
     <Grid container  >
         <Grid item xs={6}>
-        <Box sx={{ fontWeight: 'bold', m: 1,fontSize: 'h3.fontSize' }}>Our Mission</Box>
-         <Typography variant="body1" gutterBottom>
+        <Box sx={{ fontWeight: 'bold', marginTop: 5, fontSize: 'h3.fontSize',paddingLeft:'5vw' }}> Our Mission </Box>
+            <Typography  sx={{paddingLeft:'5vw'}}variant="body1" >
          In the current generation, where time is a very limited resource, it is very essential to have an application which will serve the purpose of matching the founders of upcoming startups with the investors in the most convenient approach.
         </Typography>
         </Grid>
@@ -46,20 +49,25 @@ export default function Login(){
 
     <br/><br/><br/><br/><br/><br/><Divider /><br/>
     <Grid container>
+          <Grid align="center" item xs={6}>
+
+
+            <i class="fas fa-plane-departure fa-9x licon"></i>
+
+          </Grid>
         <Grid item xs={6}>
-        <Box sx={{ fontWeight: 'bold', m: 1,fontSize: 'h3.fontSize' }}>How It Works</Box>
-         
-         <Typography variant="body1" gutterBottom>
-         Our application focuses on fulfilling this purpose with the help of a card-swiping feature. The founders will be able to post their pitch which will be displayed as cards to the investors and the investor can either left swipe to reject or right swipe to accept the match. Once there is a successful match, both the parties will be able to mutually interact with each other and go forward with the process.
+        <Box sx={{ fontWeight: 'bold', marginTop: 5, fontSize: 'h3.fontSize',paddingLeft:'4vw' }}>How It Works</Box>
+            <Typography sx={{paddingRight:'3vw',paddingLeft:'4vw'}}variant="body1" gutterBottom>
+               Our application focuses on fulfilling this purpose with the help of a card-swiping feature. The founders will be able to post their pitch which will be displayed as cards to the investors and the investor can either left swipe to reject or right swipe to accept the match. Once there is a successful match, both the parties will be able to mutually interact with each other and go forward with the process.
         </Typography>
         </Grid>
     </Grid>
 
     <br/><br/><br/><br/><br/><br/><Divider/><br/>
     <Grid container >
-        <Grid item xs={6}>
-        <Box sx={{ fontWeight: 'bold', m: 1,fontSize: 'h3.fontSize' }}>About Us</Box>
-         <Typography variant="body1" gutterBottom>
+        <Grid item md={6}>
+            <Box sx={{ fontWeight: 'bold', marginTop: 5, fontSize: 'h3.fontSize',paddingLeft:'5vw' }}>About Us</Box>
+            <Typography sx={{ paddingLeft:'5vw' }} variant="body1" gutterBottom>
           Tanishq Shaikh <br/>
           Shreya Kasturia <br/>
           Sumeet Saini <br/>
@@ -67,8 +75,9 @@ export default function Login(){
         </Typography>
         </Grid>
     </Grid>
-   </Container>  
+   </Container> 
   <Footer/>
   </div>
+  
   )  
 }
