12bb22db26e2896cde63393343dc1b7bb87cb5a0
Tanishq Shaikh <tanishq.s@somaiya.edu>
2022-03-28 21:32:00 -0400

    upload photo

--- frontend/src/components/BusinessIdea.js
+++ frontend/src/components/BusinessIdea.js
@@ -10,7 +10,8 @@ import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 import styles from "./dashboard.module.css";
-
+import { useState, useEffect } from "react";
+import axios from 'axios';
 const theme = createTheme();
 
 const businesscategory = [
@@ -63,21 +64,71 @@ const colourStyles = {
   }
 
 export default function BusinessIdea() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  console.log(selectedImage);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [id, companyName, description, companyImageUrl] = useState(null);
+  useEffect(() => {
+    if (selectedImage) {
+      setImageUrl(URL.createObjectURL(selectedImage));
+    }
+  }, [selectedImage]);
   let navigate = useNavigate();
   const routeChange = () => {
     let path = `/founderdash`;
     navigate(path);
   };
 
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   // eslint-disable-next-line no-console
+  //   console.log({
+  //     companyname: data.get("companyname"),
+  //     tags: data.get("tags"),
+  //     idea: data.get("idea"),
+  //   });
+  // };
+  const handleChange  = (event) => {
+
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      companyname: data.get("companyname"),
-      tags: data.get("tags"),
-      idea: data.get("idea"),
-    });
+  
+    // console.log(user);
+
+
+    axios.post('/api/postpitch', { 
+      "Id": this.state.id,
+      // "LastName": this.state.lname,
+      "CompanyName": this.state.companyName,
+      "Description": this.state.description,
+      "ImageUrl":   this.state.companyImageUrl     
+    })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        if (res.status === 200) {
+          // this.setState({ isSignedUp: true });
+          // localStorage.setItem(this.state.email, this.state.typeOfUser);  // after signing up, set the state to true. This will trigger a re-render
+          // this.routeChange();
+        }
+      
+      })
+      .catch(function (error) {
+        console.log(error.toJSON());
+        // alert(error);
+        if(error.response.status == 400)
+          alert("Please enter all the required information");
+      });
   };
 
   return (
@@ -148,12 +199,32 @@ export default function BusinessIdea() {
                 placeholder="Enter your business idea in not more than 10 lines"
                 multiline              
                 />
-
+              <input 
+                accept="image/*" 
+                type="file" 
+                id="select-image"
+                style={{ display: 'none' }}
+                onChange={e => setSelectedImage(e.target.files[0])}
+                
+              />
+              <label htmlFor="select-image">
+                <Button variant="contained" color="primary" component="span">
+                  Upload Image
+                </Button>
+              </label>
+              {/* <div>{imageUrl && selectedImage && (
+                <Box mt={2} textAlign="center">
+                  <div>Image Preview:</div>
+                  <img src={imageUrl} alt={selectedImage.name} height="100px" />
+                </Box>
+              )}
+              </div> */}
+              <br></br>
               <Button
                 type="submit"
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
-                onClick={routeChange}
+                onSubmit={handleSubmit}
               >
                 Submit
               </Button>
