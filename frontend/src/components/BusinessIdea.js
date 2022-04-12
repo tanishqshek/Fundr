6854e72c541083f7e7827da85419acedb7c7a2a6
Tanishq Shaikh <tanishq.s@somaiya.edu>
2022-04-12 01:09:36 -0400

    added tagsSelect page

--- frontend/src/components/BusinessIdea.js
+++ frontend/src/components/BusinessIdea.js
@@ -13,32 +13,10 @@ import { useNavigate } from "react-router-dom";
 import styles from "./dashboard.module.css";
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import { TAGS } from "../assets/tags";
 const theme = createTheme();
 
-const businesscategory = [
-  { label: 'Automotive', value: 'Automotive' },
-  { label: 'Banking', value: 'Banking' },
-  { label: 'Financial Services', value: 'Financial Service' },
-  { label: 'Cement', value: 'Cement' },
-  { label: 'Chemicals', value: 'Chemicals' },
-  { label: 'Conglomerates', value: 'Conglomerates' },
-  { label: 'Consumer Durables', value: 'Consumer Durables' },
-  { label: 'Consumer Non-Durables', value: 'Consumer Non-Durables' },
-  { label: 'Engineering', value: 'Engineering' },
-  { label: 'Food & Beverage', value: 'Food & Beverage' },
-  { label: 'Technology', value: 'Technology' },
-  { label: 'Manufacturing', value: 'Manufacturing' },
-  { label: 'Media', value: 'Media' },
-  { label: 'Metals & Mining', value: 'Metals & Mining' },
-  { label: 'Oil & Gas', value: 'Oil & Gas' },
-  { label: 'Pharmaceuticals', value: 'Pharmaceuticals' },
-  { label: 'Real Estate', value: 'Real Estate' },
-  { label: 'Services', value: 'Services' },
-  { label: 'Telecom', value: 'Telecom' },
-  { label: 'Tobacco', value: 'Tobacco' },
-  { label: 'Utilities', value: 'Utilities' },
-  { label: 'Miscellaneous', value: 'Miscellaneous' }
-];
+const businesscategory = TAGS;
 
 <CreatableSelect
   options={businesscategory}
