7a4eb6f15eb22202c083df74a2cf6a8d859c4556
Sumeet <sumeet2807a@gmail.com>
2022-04-17 18:07:14 -0400

    added cookies for Userdata test #149

--- backend/fundrtest/userdata_test.go
+++ backend/fundrtest/userdata_test.go
@@ -20,13 +20,13 @@ func TestUserdata(t *testing.T) {
 		"Password": "tan1"
 	}`)
 
-	var signup_json = []byte(`{
-		"Name":"Yo1",
-		"Username": "sumeet2807e@gmail.com",
-		"Password": "tan1",
-		"Mobile": "999999999",
-		"UserType": "Founder"
-	}`)
+	// var signup_json = []byte(`{
+	// 	"Name":"Yo1",
+	// 	"Username": "sumeet2807e@gmail.com",
+	// 	"Password": "tan1",
+	// 	"Mobile": "999999999",
+	// 	"UserType": "Founder"
+	// }`)
 
 	var userdata_json = []byte(`{
 		"name":"Yo1",
@@ -40,15 +40,17 @@ func TestUserdata(t *testing.T) {
 	w_userdata := httptest.NewRecorder()
 
 	req_signin, _ := http.NewRequest("POST", "/api/signin", bytes.NewBuffer(signin_json))
-	req_signup, _ := http.NewRequest("POST", "/api/signup", bytes.NewBuffer(signup_json))
+	// req_signup, _ := http.NewRequest("POST", "/api/signup", bytes.NewBuffer(signup_json))
 	req_postuser, _ := http.NewRequest("POST", "/api/auth/postuserdata", bytes.NewBuffer(userdata_json))
 
 	router := server.SetRouter()
 
 	model.DB_init()
-	router.ServeHTTP(w_signup, req_signup)
+	// router.ServeHTTP(w_signup, req_signup)
 	router.ServeHTTP(w_signin, req_signin)
-	router.ServeHTTP(w_signin, req_postuser)
+	cookie_val := w_signin.Result().Cookies()[0].Value
+	req_postuser.AddCookie(&http.Cookie{Name: "mysession", Value: cookie_val})
+	router.ServeHTTP(w_userdata, req_postuser)
 
 	err := json.Unmarshal(w_signup.Body.Bytes(), &resp)
 	if err != nil {
@@ -68,5 +70,5 @@ func TestUserdata(t *testing.T) {
 	log.Println(resp)
 
 	assert.Equal(t, 200, w_signin.Code)
-	assert.Equal(t, "Signed in successfully.", resp.Message)
+	assert.Equal(t, "User updated successfully.", resp.Message)
 }
