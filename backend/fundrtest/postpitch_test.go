832b41e7995ccc47fba8fee4503ea4a2fcf208f8
Sumeet <sumeet2807a@gmail.com>
2022-04-18 22:25:16 -0400

    Added unit test for postpitch

---
+++ backend/fundrtest/postpitch_test.go
@@ -0,0 +1,79 @@
+package fundrtest
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tanishqshek/Fundr/backend/model"
+	"github.com/tanishqshek/Fundr/backend/server"
+)
+
+func TestPostPitch(t *testing.T) {
+
+	var signin_json = []byte(`{
+		"Username": "testuser@gmail.com",
+		"Password": "test1"
+	}`)
+
+	var signup_json = []byte(`{
+		"Name":"Yo1",
+		"Username": "testuser@gmail.com",
+		"Password": "test1",
+		"Mobile": "999999999",
+		"UserType": "Founder"
+	}`)
+
+	var pitchdata_json = []byte(`{
+		"company_name":"test22",
+		"description":"test22",
+		"image_url":"test",
+		"tags":"abc, cde, fgh"
+	}`)
+
+	var testuser model.User_description
+
+	resp := type_resp{}
+	w_signup := httptest.NewRecorder()
+	w_signin := httptest.NewRecorder()
+	w_userdata := httptest.NewRecorder()
+
+	req_signin, _ := http.NewRequest("POST", "/api/signin", bytes.NewBuffer(signin_json))
+	req_signup, _ := http.NewRequest("POST", "/api/signup", bytes.NewBuffer(signup_json))
+	req_postpitch, _ := http.NewRequest("POST", "/api/auth/getuserdata", bytes.NewBuffer(pitchdata_json))
+
+	router := server.SetRouter()
+
+	model.DB_init()
+	model.DB.DB.First(&testuser, "Username = ?", "testuser@gmail.com")
+	if testuser.Username == "" {
+		router.ServeHTTP(w_signup, req_signup)
+	}
+
+	router.ServeHTTP(w_signin, req_signin)
+	// fmt.Println(w_signin.Result().Cookies())
+	cookie_val := w_signin.Result().Cookies()[0].Value
+	req_postpitch.AddCookie(&http.Cookie{Name: "mysession", Value: cookie_val})
+	router.ServeHTTP(w_userdata, req_postpitch)
+
+	// err := json.Unmarshal(w_signup.Body.Bytes(), &resp)
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+
+	err := json.Unmarshal(w_signin.Body.Bytes(), &resp)
+	if err != nil {
+		// log.Println(err.Error())
+	}
+
+	// json.Unmarshal(w_userdata.Body.Bytes(), &resp)
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+
+	assert.Equal(t, 200, w_signin.Code)
+	assert.Equal(t, "Signed in successfully.", resp.Message)
+}
